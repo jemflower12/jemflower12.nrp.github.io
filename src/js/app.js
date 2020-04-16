66214c8ccbb1ac89678c5a1d40af0aab3ddfe9d8
jemflower12 <63769800+jemflower12@users.noreply.github.com>
2020-04-16 18:58:03 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -54,11 +54,14 @@ button.addEventListener('click', function() {
   animate();
 });
 
-var deferredPrompt;
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
 });
 
