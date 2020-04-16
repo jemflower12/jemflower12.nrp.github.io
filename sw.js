675bea151959916b1969b2c8f9047d4d66eb2cde
jemflower12 <63769800+jemflower12@users.noreply.github.com>
2020-04-17 00:12:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,14 +25,6 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
-  event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
-});
 
 
 
