37deee239db33524ac757ee2174b7f51004173fb
jemflower12 <63769800+jemflower12@users.noreply.github.com>
2020-04-16 20:03:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('first-app')
-      .then(function(cache) {
+    caches.open('first-app').then(function(cache) {
         cache.addAll([
-          '/',
+          
           '/index.html',
           '/src/css/app.css',
           '/src/js/app.js',
-          '/manifest.json'
         ])
       })
   );
