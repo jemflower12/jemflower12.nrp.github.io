257d3e57c2e382683fdd6ee32f48921bbb04b415
jemflower12 <63769800+jemflower12@users.noreply.github.com>
2020-04-16 20:04:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('first-app').then(function(cache) {
         cache.addAll([
-          
+          '/',
           '/index.html',
           '/src/css/app.css',
           '/src/js/app.js',
